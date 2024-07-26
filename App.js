4bf2f576f8b52143d49aa6e06c1f1c94d1d9c422
joao.messias <17604189+jmessi@users.noreply.github.com>
2024-07-27 00:04:04 +0100

    improved styling, added state management for user input and goals list, added logic to display data dynamically

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [enteredGoalText, setEnteredGoalText] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	function goalInputHandler(enteredText) {
+		setEnteredGoalText(enteredText);
+	}
+
+	function addGoalHandler() {
+		setCourseGoals((currentCourseGoals) => [
+			...currentCourseGoals,
+			enteredGoalText,
+		]);
+		setEnteredGoalText('');
+	}
+
+	return (
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder='Your course goal!'
+					style={styles.textInput}
+					onChangeText={goalInputHandler}
+					value={enteredGoalText}
+				/>
+				<Button title='Add goal' onPress={addGoalHandler} />
+			</View>
+			<View style={styles.goalsContainer}>
+				{courseGoals.length ? (
+					courseGoals.map((goal, id) => <Text key={id}>{goal}</Text>)
+				) : (
+					<Text>Empty list</Text>
+				)}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	appContainer: {
+		flex: 1,
+		paddingTop: 50,
+		paddingHorizontal: 16,
+	},
+	inputContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: '#cccccc',
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: '#cccccc',
+		width: '70%',
+		marginRight: 8,
+		padding: 8,
+	},
+	goalsContainer: {
+		flex: 5,
+	},
 });
